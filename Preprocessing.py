f4a013abbfaa974defb99f9da891ec35c7a2fee8
edlym96 <edlym96@hotmail.com>
2018-08-21 16:07:59 +0100

    Changed preprocessing input to handle dataframes

--- Preprocessing.py
+++ Preprocessing.py
@@ -13,7 +13,7 @@ import os
 
 
 class Preprocessing:
-	def __init__(self, vocabulary_input=None, vocabulary_min_frequency=3, vocabulary_max_size=300,remove_stopwords="on",stemming="on",create_context="on",create_batches="on",batch_size=30):
+	def __init__(self, vocabulary_input=None, vocabulary_min_frequency=3, vocabulary_max_size=300,remove_stopwords="on",stemming="on",create_context="on",context_window_size=2,create_batches="on",batch_size=30):
 		self.vocabulary_input=vocabulary_input
 		self.remove_stopwords=remove_stopwords
 		self.stemming=stemming
@@ -28,7 +28,7 @@ class Preprocessing:
 			vocabulary = Vocabulary(vocabulary_min_frequency,vocabulary_max_size)
 		else:
 			vocabulary=vocabulary_input
-			
+
 		finding_array=[index,finding_text]
 		for index, text in finding_array:
 			finding = Finding(index, text,remove_stopwords,stemming)
@@ -36,7 +36,7 @@ class Preprocessing:
 			all_finding.append(finding)
 			all_text += finding.sentences
 		if self.create_context=="on":
-			context = Context(all_text, 2)
+			context = Context(all_text, context_window_size)
 		if self.create_context=="on" and self.create_batches=="on":
 			batch = BatchList(context.context, self.batch_size, len(vocabulary.word_list))
 		return vocabulary,batch,all_finding
